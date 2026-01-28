180a87b508ae67b3f9958dca12b552b6f36c3698
kyoohyunlee <khyhlee@naver.com>
2026-01-28 15:53:02 +0900

    train.py에서 overfitting 방지기능 off

--- medalpaca/train.py
+++ medalpaca/train.py
@@ -238,7 +238,7 @@ def main(
         save_strategy="epoch",
         output_dir=output_dir,
         save_total_limit=save_total_limit,
-        load_best_model_at_end=True if val_set_size > 0 else False,
+        load_best_model_at_end=False,
         ddp_find_unused_parameters=False if ddp else None,
         group_by_length=group_by_length,
         report_to="wandb" if use_wandb else None,
